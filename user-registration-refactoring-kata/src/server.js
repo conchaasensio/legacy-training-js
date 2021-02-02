9a5f14350a53451e976848e2ce35b398926f6c6f
Isidro Lopez <islomar@gmail.com>
2021-02-02 10:48:41 +0100

    [user-registration-refactoring-kata] Save the user in repository before returning it (WIP)

--- user-registration-refactoring-kata/src/server.js
+++ user-registration-refactoring-kata/src/server.js
@@ -16,18 +16,23 @@ app.get("/test", async (req, res) => {
 
 app.post("/users", async (req, res) => {
   const { name, email, password } = req.body;
+  const userRepository = new UserOrmRepository();
   if (password.length <= 8 || !password.includes("_")) {
     return res
       .status(StatusCodes.BAD_REQUEST)
       .json("The password is not valid!");
   }
-  console.log(new UserOrmRepository().findByEmail(email));
-  if (new UserOrmRepository().findByEmail(email) !== undefined) {
+  //console.log(userRepository.findByEmail(email));
+  if (userRepository.findByEmail(email) !== undefined) {
     return res
       .status(StatusCodes.BAD_REQUEST)
       .json("The email is already in use");
   }
-  return res.status(StatusCodes.CREATED).json({ user: new User(1, name, email, password) });
+  
+  const user = new User(1, name, email, password);
+  userRepository.save(user);
+
+  return res.status(StatusCodes.CREATED).json({ user });
 });
 
 module.exports = app;
--- user-registration-refactoring-kata/src/user_orm_repository.js
+++ user-registration-refactoring-kata/src/user_orm_repository.js
@@ -1,6 +1,10 @@
 const User = require("./user");
 
 class UserOrmRepository {
+  save(user) {
+
+  }
+
   findByEmail(email) {
     if (email === "existing@email.com") {
       return new User(1, "Pepito", "existing@email.com", "anyPassword");
