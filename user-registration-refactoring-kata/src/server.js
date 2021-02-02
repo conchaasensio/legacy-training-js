bd127b712750d9658468b3233ca39bad95eb04bd
Isidro Lopez <islomar@gmail.com>
2021-02-02 12:08:48 +0100

    [user-registration-refactoring-kata] Send a confirmation email

--- user-registration-refactoring-kata/src/server.js
+++ user-registration-refactoring-kata/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const { StatusCodes } = require("http-status-codes");
+const nodemailer = require("nodemailer");
 const User = require("./user");
-const MAX_USER_ID = 99999;
 const UserOrmRepository = require("./user_orm_repository");
 const userRepository = new UserOrmRepository();
+const MAX_USER_ID = 99999;
 
 app.use(express.json());
 
@@ -28,11 +29,28 @@ app.post("/users", async (req, res) => {
       .status(StatusCodes.BAD_REQUEST)
       .json("The email is already in use");
   }
-  
+
   const user = new User(generateUserId(), name, email, password);
   userRepository.save(user);
 
-  //TODO: send a confirmation email
+  //Send a confirmation email
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.email",
+    port: 465,
+    auth: {
+      user: "[USERNAME]",
+      pass: "[PASSWORD]",
+    },
+  });
+  // If a proper SMTP server is configured, this line could be uncommented
+  /** 
+  await transporter.sendMail({
+    from: "noreply@codium.team",
+    to: email,
+    subject: "Welcome to Codium",
+    html: "<h1>This is the confirmation email</h1>",
+  });
+  */
 
   return res.status(StatusCodes.CREATED).json({ user });
 });
