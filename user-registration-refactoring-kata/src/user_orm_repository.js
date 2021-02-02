8099abefa72498850d57122350911f8629738002
Isidro Lopez <islomar@gmail.com>
2021-02-02 10:54:59 +0100

    [user-registration-refactoring-kata] Save the user in a map inside the repository (WIP)

--- user-registration-refactoring-kata/src/user_orm_repository.js
+++ user-registration-refactoring-kata/src/user_orm_repository.js
@@ -1,15 +1,21 @@
 const User = require("./user");
 
 class UserOrmRepository {
-  save(user) {
 
+  constructor() {
+      this.users = new Map();
+  }
+
+  save(user) {
+    this.users.set(user.email, user);
+    console.log(this.users.get(user.email));
   }
 
   findByEmail(email) {
     if (email === "existing@email.com") {
       return new User(1, "Pepito", "existing@email.com", "anyPassword");
     }
-    return undefined;
+    return this.users.get(email);
   }
 }
 
