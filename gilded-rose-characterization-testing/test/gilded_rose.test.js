19a1bfbe905e870b34d88a3d66c07f3b8408d7f3
Concha Asensio <concha@codium.team>
2021-10-06 10:28:00 +0200

    It decreases item´s sellIn by 1 at the end of each day.

--- gilded-rose-characterization-testing/test/gilded_rose.test.js
+++ gilded-rose-characterization-testing/test/gilded_rose.test.js
@@ -1,11 +1,18 @@
 const { GildedRose, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("change me", function () {
+  it("does not change item´s name at the end of each day", function () {
     const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
 
     const items = gildedRose.updateQuality();
 
     expect(items[0].name).toBe("Book");
+  });
+  it("decreases item´s sellIn by 1 at the end of each day", function () {
+    const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toBe(9);
   });
 });
