be2a991eeb512fad775ea4da72e3295d3f279abd
Concha Asensio <concha@codium.team>
2022-01-21 17:51:56 +0100

    Refactor: Extract getSimpleScores method.

--- tennis-refactoring-kata/src/tennis.js
+++ tennis-refactoring-kata/src/tennis.js
@@ -1,18 +1,5 @@
 "use strict";
 
-function getAdvantageOrWinScores(m_score1, m_score2) {
-    const minusResult = m_score1 - m_score2;
-    if (minusResult === 1) {
-        return "Advantage player1";
-    } else if (minusResult === -1) {
-        return "Advantage player2";
-    } else if (minusResult >= 2) {
-        return "Win for player1";
-    } else {
-        return "Win for player2";
-    }
-}
-
 function getScore(m_score1, m_score2) {
   let score = "";
   let tempScore = 0;
@@ -21,28 +8,7 @@ function getScore(m_score1, m_score2) {
   } else if (m_score1 >= 4 || m_score2 >= 4) {
       score = getAdvantageOrWinScores(m_score1, m_score2);
   } else {
-    for (let i = 1; i < 3; i++) {
-      if (i === 1) {
-        tempScore = m_score1;
-      } else {
-        score += "-";
-        tempScore = m_score2;
-      }
-      switch (tempScore) {
-        case 0:
-          score += "Love";
-          break;
-        case 1:
-          score += "Fifteen";
-          break;
-        case 2:
-          score += "Thirty";
-          break;
-        case 3:
-          score += "Forty";
-          break;
-      }
-    }
+      score = getSimpleScores(tempScore, m_score1, score, m_score2);
   }
   return score;
 }
@@ -61,6 +27,45 @@ function getTieScores(m_score1, score) {
         default:
             score = "Deuce";
             break;
+    }
+    return score;
+}
+
+function getAdvantageOrWinScores(m_score1, m_score2) {
+    const minusResult = m_score1 - m_score2;
+    if (minusResult === 1) {
+        return "Advantage player1";
+    } else if (minusResult === -1) {
+        return "Advantage player2";
+    } else if (minusResult >= 2) {
+        return "Win for player1";
+    } else {
+        return "Win for player2";
+    }
+}
+
+function getSimpleScores(tempScore, m_score1, score, m_score2) {
+    for (let i = 1; i < 3; i++) {
+        if (i === 1) {
+            tempScore = m_score1;
+        } else {
+            score += "-";
+            tempScore = m_score2;
+        }
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
     }
     return score;
 }
