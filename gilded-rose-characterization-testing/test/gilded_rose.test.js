a934bb1a69dbfcfbd9f945124e60bca6747e3ce2
Jose Cabrera <jose@codium.team>
2022-04-22 09:14:18 +0200

    Support es6 in gilded-rose-characterization-testing

--- gilded-rose-characterization-testing/test/gilded_rose.test.js
+++ gilded-rose-characterization-testing/test/gilded_rose.test.js
@@ -1,11 +1,11 @@
-const { GildedRose, Item } = require("../src/gilded_rose");
+import { GildedRose, Item } from '../src/gilded_rose';
 
-describe("Gilded Rose", function () {
-  it("change me", function () {
-    const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
+describe('Gilded Rose', function () {
+  it('change me', function () {
+    const gildedRose = new GildedRose([new Item('Book', 10, 20)]);
 
     const items = gildedRose.updateQuality();
 
-    expect(items[0].name).toBe("Book");
+    expect(items[0].name).toBe('Book');
   });
 });
