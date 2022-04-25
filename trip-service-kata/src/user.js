fe9fdf6e467b14b0f632df192071a4f128e36f91
Luis Rovirosa Mairlot <luis@codium.team>
2022-04-25 10:41:50 +0200

    [trip-service] add user content

--- trip-service-kata/src/user.js
+++ trip-service-kata/src/user.js
@@ -1,3 +1,25 @@
 'use strict';
 
-export default class User {}
+export default class User {
+  constructor (name) {
+    this.name= name;
+    this.trips = [];
+    this.friends = [];
+  }
+
+  addFriend(friend){
+    this.friends.push(friend)
+  }
+
+  getFriends (){
+    return this.friends;
+  }
+
+  addTrip (trip) {
+    this.trips.push(trip)
+  }
+
+  getTrips(){
+    return this.trips;
+  }
+};
