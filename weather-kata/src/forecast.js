8e24606fb96f6c8ab25e963c09cb5614c2260dcb
Luis Rovirosa Mairlot <luis@codium.team>
2022-06-09 11:14:20 +0200

    [weather-kata] replace metaweather to open-meteo

--- weather-kata/src/forecast.js
+++ weather-kata/src/forecast.js
@@ -13,27 +13,64 @@ class Forecast {
         let datetime2 = new Date();
         datetime2.setDate(datetime2.getDate() + 6);
         if (datetime < datetime2) {
-            // Find the id of the city on metawheather
-            const woeid = JSON.parse((await rp(
-                'https://www.metaweather.com/api/location/search/?query=' + city)).body)[0].woeid;
+            // Find the latitude and longitude to get the prediction
+            const response = JSON.parse((await rp("https://positionstack.com/geo_api.php?query="+ city)).body);
+            const latitude = response['data'][0]['latitude'];
+            const longitude = response['data'][0]['longitude'];
 
-            // Find the predictions for the city
-            const results = JSON.parse((await rp(
-                'https://www.metaweather.com/api/location/' + woeid)).body).consolidated_weather;
-            for (const result of results) {
+            // Find the predictions for the location
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,windspeed_10m_max&current_weather=true&timezone=Europe%2FBerlin`;
+            const results = JSON.parse((await rp(url)).body);
+            for (let i = 0; i < 7; i++) {
                 // When the date is the expected
-                if (result.applicable_date == datetime.toISOString().slice(0, 10)) {
-                    // If we have to return the wind information
+                if (results["daily"]['time'][i] === datetime.toISOString().slice(0, 10)) {
                     if (wind) {
-                        return result.wind_speed;
+                        return results['daily']['windspeed_10m_max'][i];
                     } else {
-                        return result.weather_state_name;
+                        const weatherCode = results['daily']['weathercode'][i];
+
+                        return this.codeToText(weatherCode);
                     }
                 }
             }
         } else {
             return '';
         }
+    }
+
+    codeToText (weatherCode) {
+        const text = {
+            0: 'Clear sky',
+            1: 'Mainly clear, partly cloudy, and overcast',
+            2: 'Mainly clear, partly cloudy, and overcast',
+            3: 'Mainly clear, partly cloudy, and overcast',
+            45: 'Fog and depositing rime fog',
+            48: 'Fog and depositing rime fog',
+            51: 'Drizzle: Light, moderate, and dense intensity',
+            53: 'Drizzle: Light, moderate, and dense intensity',
+            55: 'Drizzle: Light, moderate, and dense intensity',
+            56: 'Freezing Drizzle: Light and dense intensity',
+            57: 'Freezing Drizzle: Light and dense intensity',
+            61: 'Rain: Slight, moderate and heavy intensity',
+            63: 'Rain: Slight, moderate and heavy intensity',
+            65: 'Rain: Slight, moderate and heavy intensity',
+            66: 'Freezing Rain: Light and heavy intensity',
+            67: 'Freezing Rain: Light and heavy intensity',
+            71: 'Snow fall: Slight, moderate, and heavy intensity',
+            73: 'Snow fall: Slight, moderate, and heavy intensity',
+            75: 'Snow fall: Slight, moderate, and heavy intensity',
+            77: 'Snow grains',
+            80: 'Rain showers: Slight, moderate, and violent',
+            81: 'Rain showers: Slight, moderate, and violent',
+            82: 'Rain showers: Slight, moderate, and violent',
+            85: 'Snow showers slight and heavy',
+            86: 'Snow showers slight and heavy',
+            95: 'Thunderstorm: Slight or moderate',
+            96: 'Thunderstorm with slight and heavy hail',
+            99: 'Thunderstorm with slight and heavy hail',
+        }[weatherCode];
+
+        return text;
     }
 }
 
