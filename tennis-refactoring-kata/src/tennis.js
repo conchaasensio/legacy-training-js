835d341f91c98fff0436dc96026aa89f204386cb
Concha Asensio <concha@codium.team>
2023-07-06 12:30:58 +0200

    Refactor: Extract getAdvantageOrWinScores method

--- tennis-refactoring-kata/src/tennis.js
+++ tennis-refactoring-kata/src/tennis.js
@@ -6,16 +6,7 @@ function getScore(m_score1, m_score2) {
   if (m_score1 === m_score2) {
     score = getTieScores(m_score1, score);
   } else if (m_score1 >= 4 || m_score2 >= 4) {
-    const minusResult = m_score1 - m_score2;
-    if (minusResult === 1) {
-      score = "Advantage player1";
-    } else if (minusResult === -1) {
-      score = "Advantage player2";
-    } else if (minusResult >= 2) {
-      score = "Win for player1";
-    } else {
-      score = "Win for player2";
-    }
+    score = getAdvantageOrWinScores(m_score1, m_score2);
   } else {
     for (let i = 1; i < 3; i++) {
       if (i === 1) {
@@ -59,6 +50,19 @@ function getTieScores(m_score1, score) {
       break;
   }
   return score;
+}
+
+function getAdvantageOrWinScores(m_score1, m_score2) {
+  const minusResult = m_score1 - m_score2;
+  if (minusResult === 1) {
+    return 'Advantage player1';
+  } else if (minusResult === -1) {
+    return 'Advantage player2';
+  } else if (minusResult >= 2) {
+    return 'Win for player1';
+  } else {
+    return 'Win for player2';
+  }
 }
 
 module.exports = getScore;
