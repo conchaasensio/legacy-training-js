2082657f99c385c74d8df51f81588d4a08b4e4cc
Concha Asensio <concha@codium.team>
2023-07-06 13:00:14 +0200

    Refactor: Extract getRegularScores method

--- tennis-refactoring-kata/src/tennis.js
+++ tennis-refactoring-kata/src/tennis.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 function getScore(m_score1, m_score2) {
   let score = '';
   let tempScore = 0;
@@ -9,28 +8,7 @@ function getScore(m_score1, m_score2) {
   } else if (isAdvantageOrWin(m_score1, m_score2)) {
     score = getAdvantageOrWinScores(m_score1, m_score2);
   } else {
-    for (let i = 1; i < 3; i++) {
-      if (i === 1) {
-        tempScore = m_score1;
-      } else {
-        score += "-";
-        tempScore = m_score2;
-      }
-      switch (tempScore) {
-        case 0:
-          score += "Love";
-          break;
-        case 1:
-          score += "Fifteen";
-          break;
-        case 2:
-          score += "Thirty";
-          break;
-        case 3:
-          score += "Forty";
-          break;
-      }
-    }
+    score = getRegularScores(tempScore, m_score1, score, m_score2);
   }
   return score;
 }
@@ -71,6 +49,32 @@ function getAdvantageOrWinScores(m_score1, m_score2) {
   } else {
     return 'Win for player2';
   }
+}
+
+function getRegularScores(tempScore, m_score1, score, m_score2) {
+  for (let i = 1; i < 3; i++) {
+    if (i === 1) {
+      tempScore = m_score1;
+    } else {
+      score += '-';
+      tempScore = m_score2;
+    }
+    switch (tempScore) {
+      case 0:
+        score += 'Love';
+        break;
+      case 1:
+        score += 'Fifteen';
+        break;
+      case 2:
+        score += 'Thirty';
+        break;
+      case 3:
+        score += 'Forty';
+        break;
+    }
+  }
+  return score;
 }
 
 module.exports = getScore;
