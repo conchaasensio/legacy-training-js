ca9580793beea8fd2d31dcb0d62d44cf61f02ca3
Concha Asensio <concha@codium.team>
2023-07-10 11:45:53 +0200

    Refactor: Improve code legibility separating advantage and win actions in 2 different methods

--- tennis-refactoring-kata/src/tennis.js
+++ tennis-refactoring-kata/src/tennis.js
@@ -3,8 +3,10 @@
 function getScore(m_score1, m_score2) {
   if (isTie(m_score1, m_score2)) {
     return getTieScores(m_score1);
-  } else if (isAdvantageOrWin(m_score1, m_score2)) {
-    return getAdvantageOrWinScores(m_score1, m_score2);
+  } else if (isAdvantage(m_score1, m_score2)) {
+    return getAdvantageScores(m_score1, m_score2);
+  } else if (isWin(m_score1, m_score2)) {
+    return getWinScores(m_score1, m_score2);
   } else {
     return getRegularScores(m_score1, m_score2);
   }
@@ -20,16 +22,27 @@ function getTieScores(m_score1) {
   return tieScores[m_score1] || 'Deuce';
 }
 
-function isAdvantageOrWin(m_score1, m_score2) {
-  return m_score1 >= 4 || m_score2 >= 4;
+function isAdvantage(m_score1, m_score2) {
+  return (m_score1 >= 4 || m_score2 >= 4) &&
+      (m_score1 - m_score2 === 1 || m_score1 - m_score2 === -1);
 }
 
-function getAdvantageOrWinScores(m_score1, m_score2) {
+function getAdvantageScores(m_score1, m_score2) {
   const minusResult = m_score1 - m_score2;
   if (minusResult === 1) {
     return 'Advantage player1';
   } else if (minusResult === -1) {
     return 'Advantage player2';
+  }
+}
+
+function isWin(m_score1, m_score2) {
+  return m_score1 >= 4 || m_score2 >= 4;
+}
+
+function getWinScores(m_score1, m_score2) {
+  const minusResult = m_score1 - m_score2;
+  if (minusResult === 1) {
   } else if (minusResult >= 2) {
     return 'Win for player1';
   } else {
