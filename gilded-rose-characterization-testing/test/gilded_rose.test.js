6f74d088ad0fd10a8f96fcdc8fefe4cde1db5f1f
Concha Asensio <concha@codium.team>
2023-07-10 12:37:09 +0200

    It decreases item´s quality by 1 at the end or each day

--- gilded-rose-characterization-testing/test/gilded_rose.test.js
+++ gilded-rose-characterization-testing/test/gilded_rose.test.js
@@ -1,11 +1,11 @@
 const { GildedRose, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("change me", function () {
+  it("decreases item´s quality by 1 at the end or each day", function () {
     const gildedRose = new GildedRose([new Item("Book", 10, 20)]);
 
     const items = gildedRose.updateQuality();
 
-    expect(items[0].name).toBe("Book");
+    expect(items[0].quality).toBe(19);
   });
 });
