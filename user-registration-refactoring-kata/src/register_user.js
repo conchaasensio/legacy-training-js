89accdc05d6d554a1a20ced094fe37647ffd1674
Concha Asensio <concha@codium.team>
2023-07-13 09:22:51 +0200

    Refactor: move all the logic to a new Use Case Class Register_user

---
+++ user-registration-refactoring-kata/src/register_user.js
@@ -0,0 +1,46 @@
+import {StatusCodes} from 'http-status-codes';
+import orm from './user_orm_repository';
+import nodemailer from 'nodemailer';
+
+export default class Register_user {
+  execute(req, res) {
+    if (req.body.password.length <= 8 || !req.body.password.includes('_')) {
+      return res.status(StatusCodes.BAD_REQUEST).
+          json('The password is not valid!');
+    }
+    if (orm.findByEmail(req.body.email) !== undefined) {
+      return res.status(StatusCodes.BAD_REQUEST).
+          json('The email is already in use');
+    }
+
+    const user = {
+      id: Math.floor(Math.random() * 99999),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    orm.save(user);
+
+    //Send a confirmation email
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.email',
+      port: 465,
+      auth: {
+        user: '[USERNAME]',
+        pass: '[PASSWORD]',
+      },
+    });
+    // If a proper SMTP server is configured, this line could be uncommented
+    /**
+     await transporter.sendMail({
+    from: "noreply@codium.team",
+    to: email,
+    subject: "Welcome to Codium",
+    html: "<h1>This is the confirmation email</h1>",
+  });
+     */
+
+    return res.status(StatusCodes.CREATED).json({user});
+  }
+}
