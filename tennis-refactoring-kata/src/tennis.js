8263af23797a18dcafbd85bb192fd5c16eb1bfdd
Concha Asensio <concha@codium.team>
2024-02-21 19:26:39 +0100

    Refactor: extract method

--- tennis-refactoring-kata/src/tennis.js
+++ tennis-refactoring-kata/src/tennis.js
@@ -1,38 +1,12 @@
 'use strict';
 
-function isTie(pointsPlayer1, pointsPlayer2) {
-  return pointsPlayer1 === pointsPlayer2;
-}
-
 function getScore(pointsPlayer1, pointsPlayer2) {
   let score = '';
   let tempScore = 0;
   if (isTie(pointsPlayer1, pointsPlayer2)) {
-    switch (pointsPlayer1) {
-      case 0:
-        score = 'Love-All';
-        break;
-      case 1:
-        score = 'Fifteen-All';
-        break;
-      case 2:
-        score = 'Thirty-All';
-        break;
-      default:
-        score = 'Deuce';
-        break;
-    }
-  } else if (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) {
-    let minusResult = pointsPlayer1 - pointsPlayer2;
-    if (minusResult === 1) {
-      score = 'Advantage player1';
-    } else if (minusResult === -1) {
-      score = 'Advantage player2';
-    } else if (minusResult >= 2) {
-      score = 'Win for player1';
-    } else {
-      score = 'Win for player2';
-    }
+    score = getTieScores(pointsPlayer1, score);
+  } else if (isAdvantageOrWin(pointsPlayer1, pointsPlayer2)) {
+    score = getAdvantageOrWinScores(pointsPlayer1, pointsPlayer2);
   } else {
     for (let i = 1; i < 3; i++) {
       if (i === 1) {
@@ -59,6 +33,45 @@ function getScore(pointsPlayer1, pointsPlayer2) {
   }
 
   return score;
+}
+
+function isTie(pointsPlayer1, pointsPlayer2) {
+  return pointsPlayer1 === pointsPlayer2;
+}
+
+function getTieScores(pointsPlayer1, score) {
+  switch (pointsPlayer1) {
+    case 0:
+      score = 'Love-All';
+      break;
+    case 1:
+      score = 'Fifteen-All';
+      break;
+    case 2:
+      score = 'Thirty-All';
+      break;
+    default:
+      score = 'Deuce';
+      break;
+  }
+  return score;
+}
+
+function isAdvantageOrWin(pointsPlayer1, pointsPlayer2) {
+  return pointsPlayer1 >= 4 || pointsPlayer2 >= 4;
+}
+
+function getAdvantageOrWinScores(pointsPlayer1, pointsPlayer2) {
+  let minusResult = pointsPlayer1 - pointsPlayer2;
+  if (minusResult === 1) {
+    return 'Advantage player1';
+  } else if (minusResult === -1) {
+    return 'Advantage player2';
+  } else if (minusResult >= 2) {
+    return 'Win for player1';
+  } else {
+    return 'Win for player2';
+  }
 }
 
 export default getScore;
